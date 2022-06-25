69b35ce1b8334803ce384af4368928612027286c
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-25 16:25:36 -0300

    Estrutura visual do projeto - Video (parte 2)

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,18 @@
-import { DiscordLogo, Lightning } from "phosphor-react";
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+
+import '@vime/core/themes/default.css'
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="OX4XGMifSv8" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
@@ -18,10 +26,10 @@ export function Video() {
             </p>
 
             <div className="flex items-center gap-4 mt-6">
-              <img 
+              <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/viniciusmb17.png" 
-                alt="" 
+                src="https://github.com/viniciusmb17.png"
+                alt=""
               />
 
               <div className="leading-relaxed">
@@ -39,11 +47,45 @@ export function Video() {
 
             <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
               <Lightning size={24} />
-              Acesse o desafio              
+              Acesse o desafio
             </a>
           </div>
         </div>
-        <div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">
+                Material complementar
+              </strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">
+                Wallpapers exclusivos
+              </strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
 
         </div>
       </div>
